efb3d034a7167ad9b7a05d4e96fe902d6c8f47e6
Rio Fujita <rio_github@rio.st>
2026-03-21 00:11:59 +0900

    aoai_proxy 0.1.2

---
+++ Formula/aoai_proxy.rb
@@ -0,0 +1,400 @@
+class AoaiProxy < Formula
+  include Language::Python::Virtualenv
+
+  desc "OpenAI-compatible proxy for Azure OpenAI using Entra ID authentication"
+  homepage "https://github.com/rioriost/aoai_proxy"
+  url "https://github.com/rioriost/aoai-proxy/releases/download/0.1.2/aoai_proxy-0.1.2.tar.gz"
+  sha256 "28f6090b85f781351fe52573775af8b3506a2757379f27f91bfe651dce962c55"
+  license "MIT"
+
+  depends_on "python@3.12"
+  resource "annotated-doc" do
+    url "https://files.pythonhosted.org/packages/1e/d3/26bf1008eb3d2daa8ef4cacc7f3bfdc11818d111f7e2d0201bc6e3b49d45/annotated_doc-0.0.4-py3-none-any.whl"
+    sha256 "571ac1dc6991c450b25a9c2d84a3705e2ae7a53467b5d111c24fa8baabbed320"
+  end
+  resource "annotated-types" do
+    url "https://files.pythonhosted.org/packages/78/b6/6307fbef88d9b5ee7421e68d78a9f162e0da4900bc5f5793f6d3d0e34fb8/annotated_types-0.7.0-py3-none-any.whl"
+    sha256 "1f02e8b43a8fbbc3f3e0d4f0f4bfc8131bcb4eebe8849b8e5c773f3a1c582a53"
+  end
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/38/0e/27be9fdef66e72d64c0cdc3cc2823101b80585f8119b5c112c2e8f5f7dab/anyio-4.12.1-py3-none-any.whl"
+    sha256 "d405828884fc140aa80a3c667b8beed277f1dfedec42ba031bd6ac3db606ab6c"
+  end
+  resource "azure-core" do
+    url "https://files.pythonhosted.org/packages/7e/d6/8ebcd05b01a580f086ac9a97fb9fac65c09a4b012161cc97c21a336e880b/azure_core-1.39.0-py3-none-any.whl"
+    sha256 "4ac7b70fab5438c3f68770649a78daf97833caa83827f91df9c14e0e0ea7d34f"
+  end
+  resource "azure-identity" do
+    url "https://files.pythonhosted.org/packages/49/9a/417b3a533e01953a7c618884df2cb05a71e7b68bdbce4fbdb62349d2a2e8/azure_identity-1.25.3-py3-none-any.whl"
+    sha256 "f4d0b956a8146f30333e071374171f3cfa7bdb8073adb8c3814b65567aa7447c"
+  end
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/9a/3c/c17fb3ca2d9c3acff52e30b309f538586f9f5b9c9cf454f3845fc9af4881/certifi-2026.2.25-py3-none-any.whl"
+    sha256 "027692e4402ad994f1c42e52a4997a9763c646b73e4096e4d5d6db8af1d6f0fa"
+  end
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/eb/56/b1ba7935a17738ae8453301356628e8147c79dbb825bcbc73dc7401f9846/cffi-2.0.0.tar.gz"
+    sha256 "44d1b5909021139fe36001ae048dbdde8214afa20200eda0f64c068cac5d5529"
+  end
+  resource "charset_normalizer" do
+    url "https://files.pythonhosted.org/packages/2a/68/687187c7e26cb24ccbd88e5069f5ef00eba804d36dde11d99aad0838ab45/charset_normalizer-3.4.6-py3-none-any.whl"
+    sha256 "947cf925bc916d90adba35a64c82aace04fa39b46b52d4630ece166655905a69"
+  end
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/98/78/01c019cdb5d6498122777c1a43056ebb3ebfeef2076d9d026bfe15583b2b/click-8.3.1-py3-none-any.whl"
+    sha256 "981153a64e25f12d547d3426c367a4857371575ee7ad18df2a6183ab0545b2a6"
+  end
+  resource "cryptography" do
+    url "https://files.pythonhosted.org/packages/60/04/ee2a9e8542e4fa2773b81771ff8349ff19cdd56b7258a0cc442639052edb/cryptography-46.0.5.tar.gz"
+    sha256 "abace499247268e3757271b2f1e244b36b06f8515cf27c4d49468fc9eb16e93d"
+  end
+  resource "fastapi" do
+    url "https://files.pythonhosted.org/packages/e4/72/42e900510195b23a56bde950d26a51f8b723846bfcaa0286e90287f0422b/fastapi-0.135.1-py3-none-any.whl"
+    sha256 "46e2fc5745924b7c840f71ddd277382af29ce1cdb7d5eab5bf697e3fb9999c9e"
+  end
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/04/4b/29cac41a4d98d144bf5f6d33995617b185d14b22401f75ca86f384e87ff1/h11-0.16.0-py3-none-any.whl"
+    sha256 "63cf8bbe7522de3bf65932fda1d9c2772064ffb3dae62d55932da54b31cb6c86"
+  end
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/7e/f5/f66802a942d491edb555dd61e3a9961140fd64c90bce1eafd741609d334d/httpcore-1.0.9-py3-none-any.whl"
+    sha256 "2d400746a40668fc9dec9810239072b40b4484b640a8c38fd654a024c7a1bf55"
+  end
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/2a/39/e50c7c3a983047577ee07d2a9e53faf5a69493943ec3f6a384bdc792deb2/httpx-0.28.1-py3-none-any.whl"
+    sha256 "d909fcccc110f8c7faf814ca82a9a4d816bc5a6dbfea25d6591d6985b8ba59ad"
+  end
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/0e/61/66938bbb5fc52dbdf84594873d5b51fb1f7c7794e9c0f5bd885f30bc507b/idna-3.11-py3-none-any.whl"
+    sha256 "771a87f49d9defaf64091e6e6fe9c18d4833f140bd19464795bc32d966ca37ea"
+  end
+  resource "msal" do
+    url "https://files.pythonhosted.org/packages/96/86/16815fddf056ca998853c6dc525397edf0b43559bb4073a80d2bc7fe8009/msal-1.35.1-py3-none-any.whl"
+    sha256 "8f4e82f34b10c19e326ec69f44dc6b30171f2f7098f3720ea8a9f0c11832caa3"
+  end
+  resource "msal-extensions" do
+    url "https://files.pythonhosted.org/packages/5e/75/bd9b7bb966668920f06b200e84454c8f3566b102183bc55c5473d96cb2b9/msal_extensions-1.3.1-py3-none-any.whl"
+    sha256 "96d3de4d034504e969ac5e85bae8106c8373b5c6568e4c8fa7af2eca9dbe6bca"
+  end
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/0c/c3/44f3fbbfa403ea2a7c779186dc20772604442dde72947e7d01069cbe98e3/pycparser-3.0-py3-none-any.whl"
+    sha256 "b727414169a36b7d524c1c3e31839a521725078d7b2ff038656844266160a992"
+  end
+  resource "pydantic" do
+    url "https://files.pythonhosted.org/packages/5a/87/b70ad306ebb6f9b585f114d0ac2137d792b48be34d732d60e597c2f8465a/pydantic-2.12.5-py3-none-any.whl"
+    sha256 "e561593fccf61e8a20fc46dfc2dfe075b8be7d0188df33f221ad1f0139180f9d"
+  end
+  resource "pydantic-core" do
+    if OS.mac? && Hardware::CPU.arm?
+      url "https://files.pythonhosted.org/packages/aa/32/9c2e8ccb57c01111e0fd091f236c7b371c1bccea0fa85247ac55b1e2b6b6/pydantic_core-2.41.5-cp312-cp312-macosx_11_0_arm64.whl"
+      sha256 "070259a8818988b9a84a449a2a7337c7f430a22acc0859c6b110aa7212a6d9c0"
+    elsif OS.mac? && Hardware::CPU.intel?
+      url "https://files.pythonhosted.org/packages/5f/5d/5f6c63eebb5afee93bcaae4ce9a898f3373ca23df3ccaef086d0233a35a7/pydantic_core-2.41.5-cp312-cp312-macosx_10_12_x86_64.whl"
+      sha256 "f41a7489d32336dbf2199c8c0a215390a751c5b014c2c1c5366e817202e9cdf7"
+    elsif OS.linux?
+      url "https://files.pythonhosted.org/packages/0d/76/941cc9f73529988688a665a5c0ecff1112b3d95ab48f81db5f7606f522d3/pydantic_core-2.41.5-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+      sha256 "eceb81a8d74f9267ef4081e246ffd6d129da5d87e37a77c9bde550cb04870c1c"
+    else
+      url "https://files.pythonhosted.org/packages/71/70/23b021c950c2addd24ec408e9ab05d59b035b39d97cdc1130e1bce647bb6/pydantic_core-2.41.5.tar.gz"
+      sha256 "08daa51ea16ad373ffd5e7606252cc32f07bc72b28284b6bc9c6df804816476e"
+    end
+  end
+  resource "pydantic-settings" do
+    url "https://files.pythonhosted.org/packages/00/4b/ccc026168948fec4f7555b9164c724cf4125eac006e176541483d2c959be/pydantic_settings-2.13.1-py3-none-any.whl"
+    sha256 "d56fd801823dbeae7f0975e1f8c8e25c258eb75d278ea7abb5d9cebb01b56237"
+  end
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/0b/d7/1959b9648791274998a9c3526f6d0ec8fd2233e4d4acce81bbae76b44b2a/python_dotenv-1.2.2-py3-none-any.whl"
+    sha256 "1d8214789a24de455a8b8bd8ae6fe3c6b69a5e3d64aa8a8e5d68e694bbcb285a"
+  end
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/1e/db/4254e3eabe8020b458f1a747140d32277ec7a271daf1d235b70dc0b4e6e3/requests-2.32.5-py3-none-any.whl"
+    sha256 "2462f94637a34fd532264295e186976db0f5d453d1cdd31473c85a6a161affb6"
+  end
+  resource "starlette" do
+    url "https://files.pythonhosted.org/packages/81/0d/13d1d239a25cbfb19e740db83143e95c772a1fe10202dda4b76792b114dd/starlette-0.52.1-py3-none-any.whl"
+    sha256 "0029d43eb3d273bc4f83a08720b4912ea4b071087a3b48db01b7c839f7954d74"
+  end
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/18/67/36e9267722cc04a6b9f15c7f3441c2363321a3ea07da7ae0c0707beb2a9c/typing_extensions-4.15.0-py3-none-any.whl"
+    sha256 "f0fa19c6845758ab08074a0cfa8b7aecb71c999ca73d62883bc25cc018c4e548"
+  end
+  resource "typing-inspection" do
+    url "https://files.pythonhosted.org/packages/dc/9b/47798a6c91d8bdb567fe2698fe81e0c6b7cb7ef4d13da4114b41d239f65d/typing_inspection-0.4.2-py3-none-any.whl"
+    sha256 "4ed1cacbdc298c220f1bd249ed5287caa16f34d44ef4e9c3d0cbad5b521545e7"
+  end
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/39/08/aaaad47bc4e9dc8c725e68f9d04865dbcb2052843ff09c97b08904852d84/urllib3-2.6.3-py3-none-any.whl"
+    sha256 "bf272323e553dfb2e87d9bfd225ca7b0f467b919d7bbd355436d3fd37cb0acd4"
+  end
+  resource "uvicorn" do
+    url "https://files.pythonhosted.org/packages/0a/89/f8827ccff89c1586027a105e5630ff6139a64da2515e24dafe860bd9ae4d/uvicorn-0.42.0-py3-none-any.whl"
+    sha256 "96c30f5c7abe6f74ae8900a70e92b85ad6613b745d4879eb9b16ccad15645359"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3.12")
+
+    resource("annotated-doc").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("annotated-types").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("anyio").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("azure-core").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("azure-identity").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("certifi").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("cffi").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("charset_normalizer").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("click").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("cryptography").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("fastapi").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("h11").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("httpcore").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("httpx").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("idna").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("msal").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("msal-extensions").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("pycparser").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("pydantic").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("pydantic-core").stage do
+      if OS.mac? && Hardware::CPU.arm?
+        venv.pip_install Pathname(Dir["*.whl"].first)
+      elsif OS.mac? && Hardware::CPU.intel?
+        venv.pip_install Pathname(Dir["*.whl"].first)
+      elsif OS.linux?
+        venv.pip_install Pathname(Dir["*.whl"].first)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("pydantic-settings").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("python-dotenv").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("requests").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("starlette").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("typing-extensions").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("typing-inspection").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("urllib3").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    resource("uvicorn").stage do
+      wheel = Dir["*.whl"].first
+      if wheel
+        venv.pip_install Pathname(wheel)
+      else
+        venv.pip_install Pathname.pwd
+      end
+    end
+
+    venv.pip_install buildpath
+    bin.install_symlink libexec/"bin/aoai-proxy"
+  end
+
+  test do
+    system "#{bin}/aoai-proxy", "--help"
+  end
+end

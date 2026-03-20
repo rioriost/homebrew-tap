b89bce4afc340b872fc480886f3ea84f0b67d8d2
Rio Fujita <rio_github@rio.st>
2026-03-20 11:00:54 +0900

    envrcctl 0.2.2

--- Formula/envrcctl.rb
+++ Formula/envrcctl.rb
@@ -3,8 +3,8 @@ class Envrcctl < Formula
 
   desc "Manage .envrc with managed blocks and OS-backed secrets"
   homepage "https://github.com/rioriost/envrcctl"
-  url "https://github.com/rioriost/envrcctl/releases/download/0.2.2/envrcctl-0.2.2.tar.gz"
-  sha256 "b4f0afb5d3cd1d861918e9564d230aa38e5dd1c08547d5e2000d4817b8705e80"
+  url "https://files.pythonhosted.org/packages/f3/d6/8b8109fa5479df987b66db36d8904afd95e8652c57e98db56d4eeeffdcce/envrcctl-0.2.2.tar.gz"
+  sha256 "0f35644f150ef9dd75117d0bfdb9fa6a42f4fffca99166b325c83076f66b4783"
   license "MIT"
 
   depends_on arch: :arm64
@@ -13,8 +13,7 @@ class Envrcctl < Formula
 
   resource "envrcctl-macos-auth" do
     url "https://github.com/rioriost/envrcctl/releases/download/0.2.2/envrcctl-macos-auth-0.2.2-arm64.tar.gz"
-    sha256 "c8abd30083608d8fbc2c6f0b3a475f9fbc35f88c6c095cee1f9fb0b6908b797e"
-  end
+    sha256 "c8abd30083608d8fbc2c6f0b3a475f9fbc35f88c6c095cee1f9fb0b6908b797e"  end
 
   def install
     virtualenv_install_with_resources

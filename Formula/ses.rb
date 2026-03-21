5d05eb07e5ef68669ac345c292404e4d7b13726a
Rio Fujita <rio_github@rio.st>
2026-03-21 22:36:47 +0900

    brew: bump ses formula to 0.0.5

--- Formula/ses.rb
+++ Formula/ses.rb
@@ -1,9 +1,9 @@
 class Ses < Formula
   desc "Speech Event Stream CLI"
   homepage "https://github.com/rioriost/ses"
-  url "https://github.com/rioriost/ses/releases/download/v0.0.3/ses-0.0.3-macos.tar.gz"
-  sha256 "14d8ee0c19f1af056933a9693cf1e4622b6ce0aff5f64b5d3fcd1e0897494d8d"
-  version "0.0.3"
+  url "https://github.com/rioriost/ses/releases/download/0.0.5/ses-0.0.5-macos.tar.gz"
+  sha256 "3a0e7b270f9b6ee0c3927e2b5ca3aba2a1ddcb60e55ca1c5dbbc8bfc0008638a"
+  version "0.0.5"
 
   def install
     bin.install "ses"

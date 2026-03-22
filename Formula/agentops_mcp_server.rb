c4effc78040c7d78694892d4f319d656d70679f6
Rio Fujita <rio_github@rio.st>
2026-03-22 10:31:04 +0900

    agentops_mcp_server 0.5.4

--- Formula/agentops_mcp_server.rb
+++ Formula/agentops_mcp_server.rb
@@ -1,18 +1,18 @@
 class AgentopsMcpServer < Formula
   include Language::Python::Virtualenv
 
-  desc "AgentOps MCP Server"
-  homepage "https://github.com/rioriost/agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/ff/8b/04ef2ca6731a4c94d591021a64c28dafdade50f187b9d89a828876d9b875/agentops_mcp_server-0.5.3.tar.gz"
-  sha256 "4bda3d62da07ba3e72bbc35abd2689fb067ed7003f31d007fed01c1498030912"
+  desc "Simple MCP server for AgentOps with Zed"
+  homepage "https://github.com/rioriost/agentops_mcp_server"
+  url "https://github.com/rioriost/agentops_mcp_server/releases/download/0.5.4/agentops_mcp_server-0.5.4.tar.gz"
+  sha256 "f5af5564dc520f01f4c7dc3977a94bc98dbde7f4ef4d1c534443602e6d549ac2"
   license "MIT"
 
   depends_on "python@3.14"
 
   def install
-    virtualenv_install_with_resources
-    prefix.install "README-jp.md"
-    prefix.install "docs"
+    cd "." do
+      virtualenv_install_with_resources
+    end
   end
 
   test do
